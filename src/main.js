a4f7e8b500238dfbef701477fb4e922b4eccbc77
Christie Knudson <christie.knudson@Christies-iMac.local>
2018-11-24 21:16:02 -0600

    Use appendchild method

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
-const forecast = {"error": "404"};
+let forecast = {"error": "404"};
 const weatherWrapper = document.getElementsByClassName("span");
 const currentWeather = document.createTextNode(forecast.error);
+//forecast.weather[0].description
+weatherWrapper.appendChild(currentWeather);
