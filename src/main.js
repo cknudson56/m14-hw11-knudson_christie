4b405095345ddc5071107cd9fac41e754749bbe2
Christie Knudson <christie.knudson@Christies-iMac.local>
2018-11-24 21:10:08 -0600

    Add text nodeinside of current weather

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
 const forecast = {"error": "404"};
-const weatherWrapper = document.getElementsByClassName('span');
-const currentWeather = document.createTextNode();
+const weatherWrapper = document.getElementsByClassName("span");
+const currentWeather = document.createTextNode(forecast.error);
